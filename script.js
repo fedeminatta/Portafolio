44a6a11df5e4d4e4e43852a15692563ca1108d31
fedeminatta <minattafederico@gmail.com>
2022-12-26 20:41:07 -0300

    darkmode

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 // agregar modo oscuro
+let dark = document.querySelector('#dark');
+let rootStyle = document.documentElement.style;
+dark.addEventListener('click', darkMode);
+function darkMode() {
+	if (dark.classList.contains('fa-sun-o')) {
+		dark.classList.remove('fa-sun-o');
+		dark.classList.add('fa-moon-o');
+		rootStyle.setProperty('--bg-primary', '#191919');
+		rootStyle.setProperty('--bg-secundary', '#202020');
+		rootStyle.setProperty('--text', '#fff');
+	} else if (dark.classList.contains('fa-moon-o')) {
+		dark.classList.remove('fa-moon-o');
+		dark.classList.add('fa-sun-o');
+		rootStyle.setProperty('--bg-primary', '#f7f7ee');
+		rootStyle.setProperty('--bg-secundary', '#ece9e1');
+		rootStyle.setProperty('--text', '#000');
+	}
+}
 
 // Carruzel
 document.addEventListener('DOMContentLoaded', () => {
