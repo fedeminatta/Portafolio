29c789faf234f8a2ac5ba16af4880ed643b241f6
fedeminatta <minattafederico@gmail.com>
2022-12-26 18:14:36 -0300

    s

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 
 // Carruzel
 document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel');
-    M.Carousel.init(elementosCarousel, {
-        duration: 150,
-        indicators: true,
-    });
+	const elementosCarousel = document.querySelectorAll('.carousel');
+	M.Carousel.init(elementosCarousel, {
+		duration: 150,
+		indicators: true,
+	});
+});
+
+// validar formulario
+let form = document.querySelector('#form');
+let enviado = document.querySelector('.enviado');
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	enviado.style.left = '0';
+	setTimeout(() => {
+		enviado.style.left = '-1000px';
+	}, 3000);
 });
